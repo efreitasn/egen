e1df1ba0fd9c241baf82105185cd841fef8c528f
Emanuel Freitas <efreitasn@hotmail.com>
2020-02-15 01:03:52 -0300

    Add viewport meta tag to base template

--- templates.go
+++ templates.go
@@ -19,7 +19,8 @@ var indexHTML = `
 <!DOCTYPE html>
 <html lang="{{ .Lang.Tag }}">
 <head>
-  <meta charset="utf-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>{{ .Title }}</title>
 	{{ if .Description }}
 		<meta property="description" content="{{ .Description }}">
